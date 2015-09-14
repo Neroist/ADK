8f7a22d900d5e1bd52d0551d990bc2b6861ffe3d
Neroist <wbolin@kth.se>
2015-09-14 16:51:19 +0200

    skal för search

---
+++ lab1/src/Konkordans.java
@@ -0,0 +1,23 @@
+
+public class Konkordans {
+	
+	public static void main(String[] args) {
+		//TODO
+	}
+	private long binSearch(String word){
+		//boolean? måste kunna visa om något ord inte finns med.
+	}
+	private int tlHash(String threeLetter){
+		//TODO
+	}
+	private long getIndexPosition(String word){
+		//TODO
+	}
+	private long[] getWordPositions(long indexPosition){
+		//TODO
+	}
+	private void showAllOccurrences(long[] wordPositions){
+		//TODO
+	}
+	
+}
