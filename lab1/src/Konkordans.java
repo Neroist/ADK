7f9636d733728c911b03b14001682ce4a573dfb9
Sam Andren <samand@kth.se>
2015-09-15 14:09:04 +0200

    LetterConverter class added

--- lab1/src/Konkordans.java
+++ lab1/src/Konkordans.java
@@ -3,18 +3,22 @@ public class Konkordans {
 	
 	public static void main(String[] args) {
 		//TODO
+		int wordHash;
+		wordHash = tlHash("aaa");
+		System.out.println(wordHash);
 	}
 	private long binSearch(String word){
 		//boolean? måste kunna visa om något ord inte finns med.
+		return null;
 	}
-	private int tlHash(String threeLetter){
-		//TODO
-	}
+
 	private long getIndexPosition(String word){
 		//TODO
+		return null;
 	}
 	private long[] getWordPositions(long indexPosition){
 		//TODO
+		return null;
 	}
 	private void showAllOccurrences(long[] wordPositions){
 		//TODO
